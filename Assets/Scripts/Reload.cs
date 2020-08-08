aa82a1a289840a527a569159ff659c837bd96a20
Wolfleader <james.morgan@iinet.net.au>
2020-08-08 19:19:10 +0800

    fixes

--- Assets/Scripts/Reload.cs
+++ Assets/Scripts/Reload.cs
@@ -1,15 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Reload : MonoBehaviour
 {
-    
     public Image reloadImage;
     [Range(0, 1)]
     public float reloadProgress = 0;
-    
+        
     // Start is called before the first frame update
     void Start()
     {
