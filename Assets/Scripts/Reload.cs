595b784251f4c4c9cc3bca76dea6b6e2ef0c3e6d
Wolfleader <james.morgan@iinet.net.au>
2020-08-08 09:35:02 +0800

    reloading in progress

---
+++ Assets/Scripts/Reload.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reload : MonoBehaviour
+{
+    
+    public Image reloadImage;
+    [Range(0, 1)]
+    public float reloadProgress = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reloadImage.fillAmount += 0.00086f;
+    }
+}
